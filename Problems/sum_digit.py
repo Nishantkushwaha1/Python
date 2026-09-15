2c2e19fc8d8fb838dd5ad46604a611eb4a3370cf
Nishant Kushwaha <Nishantkushwahadeoria@gmail.com>
2026-09-15 21:32:31 +0530

    sum of digits

---
+++ Problems/sum_digit.py
@@ -0,0 +1,18 @@
+
+# Sum of digits
+
+n = int(input("Enter the number: "))
+
+sum = 0
+
+while n>0:
+    digit = n%10
+    sum = sum + digit
+    n= n//10
+print(sum)
+
+# Ouput:
+# Enter the number: 123
+# 6
+
+
